0b6b90605d4184cebe9f9fca2ead0f2423da169e
matiasecharri <matiasecharri@hotmail.com>
2022-10-26 14:13:46 -0300

    Se agregron estilos a las cartas, todavía no me terminan de convencer, puede que haya algun otro cambio proximamente, ademas se agregaron estilos a los placeholder, si así es eso es lo que se hizo, fue poco? quizás no lo se, pero alguien tenía que hacerlo

--- js/juguetes.js
+++ js/juguetes.js
@@ -70,39 +70,47 @@ function actulizarValueRange(){
 
 function imprimirArticulos(contenedor, array) {
   for (const objeto of array) {
-    if (objeto.stock < 5) {
+    if (objeto.stock < 3) {
       contenedor.innerHTML += `
-    
-    <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
-    <p class="barraStock text-center bg-danger text-white">Queda(n) ${objeto.stock} en stock</p>
-        <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-        <div class="card-body d-flex flex-column align-items-around justify-content-center">                
-                <h5 class="card-title text-center">${objeto.nombre}</h5>
-                <h5 class="card-title text-center">$${objeto.precio}</h5>                
-                </div>
-                <div class="botones d-flex mb-3 flex-row justify-content-evenly">
-                    <a href="#" class="btn btn-primary">Detalles</a>
-                    <a href="#" class="btn btn-primary">Agregar al carrito</a>
-                </div>
-    </article>
-    
-    `;
-    } else {
-      contenedor.innerHTML += `
-    
-        <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
-            <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-            <div class="card-body d-flex flex-column align-items-around justify-content-center">                
-                    <h5 class="card-title text-center">${objeto.nombre}</h5>
-                    <h5 class="card-title text-center">$${objeto.precio}</h5>                
-                    </div>
-                    <div class="botones d-flex mb-3 flex-row justify-content-evenly">
-                        <a href="./detalles.html" class="btn btn-primary">Detalles</a>
-                        <a href="#" class="btn btn-primary">Agregar al carrito</a>
-                    </div>
-        </article>
         
-        `;
+      <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
+      <p class="barraStock text-center bg-danger text-white">Queda(n) ${objeto.stock} en stock</p>
+          <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
+          <div class="card-body d-flex flex-column align-items-around justify-content-center">   
+          <hr> 
+          <div class="textoCard">
+          <h5 class="card-title text-center">${objeto.nombre}</h5>
+          <h5 class="card-title text-center">$${objeto.precio}</h5>     
+         </div>           
+                  </div>
+                  <div class="botones d-flex mb-3 flex-row justify-content-evenly">
+                      <a href="#" class="btn btn-primary">Detalles</a>
+                      <a href="#" class="btn btn-primary">Agregar al carrito</a>
+                  </div>
+      </article>
+      
+      `
+      } else {
+          contenedor.innerHTML += `
+      
+          <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
+          <div><p class="barraStockConStock text-center text-white"></p>
+              <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
+              <div class="card-body d-flex flex-column align-items-around justify-content-center">  
+              <hr>
+                         
+                      <div class="textoCard">
+        <h5 class="card-title text-center">${objeto.nombre}</h5>
+        <h5 class="card-title text-center">$${objeto.precio}</h5>     
+       </div>             
+                      </div>
+                      <div class="botones d-flex mb-3 flex-row justify-content-evenly">
+                          <a href="#" class="btn btn-primary">Detalles</a>
+                          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+                      </div>
+          </article>
+          
+          `;
     }
   }
 }
