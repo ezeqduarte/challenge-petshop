30ab7926920542ce07fe61f16ecbfadccac71fc3
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-10-26 18:38:17 -0300

    Cambie las tarjetas en el js de carrito y acomode algunos estilos mas

--- js/carrito.js
+++ js/carrito.js
@@ -10,35 +10,49 @@ function imprimirArticulos(contenedor, array) {
     for (const objeto of array) {
         if (objeto.stock < 5) {
             contenedor.innerHTML += `
-      
-      <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
-      <p class="barraStock text-center bg-danger text-white">Queda(n) ${objeto.stock} en stock</p>
-          <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-          <div class="card-body d-flex flex-column align-items-around justify-content-center">                
-                  <h5 class="card-title text-center">${objeto.nombre}</h5>
-                  <h5 class="card-title text-center">$${objeto.precio}</h5>                
-                  </div>
-                  <div class="botones d-flex mb-3 flex-row justify-content-evenly">
-                      <a href="#" class="btn btn-primary">Detalles</a>
-                      <a  class="btn btn-primary" id="${objeto.nombre}" name="${objeto.precio}" >Agregar al carrito</a>
-                  </div>
-      </article>
+            <div class="tarjeta">
+            <div class="contenido">
+                <div class="ladoIzq d-flex justify-content-center align-items-center flex-column">
+                <h2 class="titulo text-center">"${objeto.nombre}"</h2>
+                <div class="cuerpo">
+                <p class="text-center">Este es un texto que pretende describir o presentar el artículo, la información o los datos que queramos presentar.</p>
+                <div class="d-flex justify-content-evenly">
+                      <button class="btn">Eliminar del carrito</button>
+                      <a class="btn" href="index.html">Volver al inicio</a>
+                
+                      </div>
+                      <div class="d-flex justify-content-center text-center">
+      <p class="barraStock barrastockCarrito text-white mt-5 text-center">Queda(n) ${objeto.stock} en stock</p>
+</div>
+            </div>
+             </div>
+              <div class="ladoDer">
+                 <img src="${objeto.imagen}" alt="${objeto.nombre}"/></div>
+              </div>
+
+          </div>
+          
       
       `;
         } else {
             contenedor.innerHTML += `
       
-          <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
-              <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-              <div class="card-body d-flex flex-column align-items-around justify-content-center">                
-                      <h5 class="card-title text-center">${objeto.nombre}</h5>
-                      <h5 class="card-title text-center">$${objeto.precio}</h5>                
-                      </div>
-                      <div class="botones d-flex mb-3 flex-row justify-content-evenly">
-                          <a href="./detalles.html" class="btn btn-primary">Detalles</a>
-                          <a    class="btn btn-primary"  id="${objeto.nombre}" username="${objeto.precio}">Agregar al carrito</a>
-                      </div>
-          </article>
+            <div class="tarjeta">
+            <div class="contenido">
+                <div class="ladoIzq d-flex justify-content-center align-items-center flex-column">
+                <h2 class="titulo text-center">"${objeto.nombre}"</h2>
+                <div class="cuerpo">
+                <p class="text-center">Este es un texto que pretende describir o presentar el artículo, la información o los datos que queramos presentar.</p>
+                <div class="d-flex justify-content-evenly">
+                      <button class="btn">Eliminar del carrito</button>
+                      <a class="btn" href="index.html">Volver al inicio</a>
+                </div>
+            </div>
+             </div>
+              <div class="ladoDer">
+                 <img src="${objeto.imagen}" alt="${objeto.nombre}"/></div>
+              </div>
+          </div>
           
           `;
         }
