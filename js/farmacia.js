518c6630ffec5b3314abf520d36954d444d2d167
HernanC95 <cavalieri.h95@gmail.com>
2022-10-27 09:17:46 -0300

    update-Filter

--- js/farmacia.js
+++ js/farmacia.js
@@ -1,3 +1,12 @@
+/* import { Carro } from './carro.js' */
+/* let oCarro = new Carro(); */
+let valueRangeMin;
+let valueRangeMax;
+let arrayFiltroPorRango;
+let btn_carro
+let articles
+let arrayFiltroPorPalabra;
+
 let $container_cards = document.getElementById("contenedor_cards")
 
 async function getData() {
@@ -10,11 +19,12 @@ async function getData() {
         let infoApi = data.response
 
         let articles = [...infoApi].filter(article=> article.tipo === "Medicamento").sort((a,b)=> a.stock - b.stock) 
-               
-
+        let carro = localStorage.getItem('carro')
+        console.log(articles);
         imprimirArticulos($container_cards, articles)
 
         filtrosCruzados ($container_cards,articles)
+        btn_carro = document.querySelectorAll(`[class^="btn btn-primary"]`);
     } catch (error) {
         
         console.log(error);
@@ -22,7 +32,6 @@ async function getData() {
     }
 
 }
-
 
 const inputSearch = document.getElementById("js-search");
 inputSearch.addEventListener("input", getData);
@@ -60,57 +69,31 @@ function filterByRange(medicamentos) {
         contenedor.innerHTML = ``;
         imprimirArticulos(contenedor,arrayFiltroPorPalabra);
       }
-      
-      inputSearchMinValue.value = inputRangeMin.value
-      inputSearchMaxValue.value = inputRangeMax.value
+      actulizarValueRange()
 
 }
+valueRangeMin = document.getElementById("valueRangeMin")
+valueRangeMax = document.getElementById("valueRangeMax")
 
+function actulizarValueRange() {
+    valueRangeMin.innerHTML = `$ ${Number(inputRangeMin.value)}`
+    valueRangeMax.innerHTML = `$ ${Number(inputRangeMax.value)}`
 
-inputSearchMinValue = document.getElementById("js-searchMin")
-inputSearchMinValue.addEventListener("input",actualizarRangeMin)
-
-function actualizarRangeMin (){
-    if(inputSearchMinValue.value === ""){
-        inputRangeMin.value = 0
-    }
-    else{
-        inputRangeMin.value = inputSearchMinValue.value
-    }
-    console.log(inputSearchMinValue);
-    getData()
-}
-
-inputSearchMaxValue = document.getElementById("js-searchMax")
-inputSearchMaxValue.addEventListener("input",actualizarRangeMax)
-
-function actualizarRangeMax (){
-    if(inputSearchMaxValue.value === ""){
-        inputRangeMax.value = 0
-    }
-    else{
-        inputRangeMax.value = inputSearchMaxValue.value
-    }
-    getData()
-}
-
+  }
 
 
 function imprimirArticulos(contenedor, array) {
        
     for (const objeto of array) {
-        if (objeto.stock<3) {
+        if (objeto.stock<5) {
             contenedor.innerHTML += `
         
         <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
         <p class="barraStock text-center bg-danger text-white">Queda(n) ${objeto.stock} en stock</p>
             <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-            <div class="card-body d-flex flex-column align-items-around justify-content-center">   
-            <hr> 
-            <div class="textoCard">
-            <h5 class="card-title text-center">${objeto.nombre}</h5>
-            <h5 class="card-title text-center">$${objeto.precio}</h5>     
-           </div>           
+            <div class="card-body d-flex flex-column align-items-around justify-content-center">                
+                    <h5 class="card-title text-center">${objeto.nombre}</h5>
+                    <h5 class="card-title text-center">$${objeto.precio}</h5>                
                     </div>
                     <div class="botones d-flex mb-3 flex-row justify-content-evenly">
                         <a href="#" class="btn btn-primary">Detalles</a>
@@ -123,15 +106,10 @@ function imprimirArticulos(contenedor, array) {
             contenedor.innerHTML += `
         
             <article class="card d-flex flex-column align-items-around" style="width: 18rem;">
-            <div><p class="barraStockConStock text-center text-white"></p>
                 <img class="card-img-top" src="${objeto.imagen}" alt="${objeto.nombre}">
-                <div class="card-body d-flex flex-column align-items-around justify-content-center">  
-                <hr>
-                           
-                        <div class="textoCard">
-          <h5 class="card-title text-center">${objeto.nombre}</h5>
-          <h5 class="card-title text-center">$${objeto.precio}</h5>     
-         </div>             
+                <div class="card-body d-flex flex-column align-items-around justify-content-center">                
+                        <h5 class="card-title text-center">${objeto.nombre}</h5>
+                        <h5 class="card-title text-center">$${objeto.precio}</h5>                
                         </div>
                         <div class="botones d-flex mb-3 flex-row justify-content-evenly">
                             <a href="#" class="btn btn-primary">Detalles</a>
@@ -147,3 +125,18 @@ function imprimirArticulos(contenedor, array) {
 }
 
 getData()
+
+function FilterByname(data, string) {
+    return data.filter(data => data.nombre.toLowerCase().trim().includes(string.toLowerCase().trim()));
+  }
+  
+  
+  setTimeout(() => {
+    btn_carro.forEach(e => e.addEventListener("click", () => {
+      console.dir(e.id);
+      oCarro.agregaCarro(FilterByname(articles, e.id));
+  
+    }));
+  
+  }, 1000);
+  
